4d0c4f09204517a26dadf5cb88c6ec4b13e75899
beautywd <beautyakter1891994@gmail.com>
2024-12-22 21:45:42 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,13 @@
+
+let address ={
+
+    "name":"beauty akter",
+   "village": "jhallaum",
+   "post":"Goraihut",
+   "union":"durgapur",
+   "upozalla":"Ulipur",
+   "zalla":"Kurigram",
+    "phone": "01753722324",
+
+}
+console.log(address);
