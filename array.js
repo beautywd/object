2262ea4a52a861a34cd4d049a2602fed48569764
beautywd <beautyakter1891994@gmail.com>
2024-12-27 00:26:01 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,50 @@
+let abc =(a,b)=>{                    // arroy function
+    console.log(a,"+",b,"=",a+b);
+}
+
+abc(5,6)
+
+let xyz =(a,b)=>{
+    console.log(a,"-",b,"=",a-b);
+}
+xyz(8,6)
+
+
+let name =(a,b)=>{
+    console.log(a,"*",b,"=", a*b);
+}
+name(6,6)
+
+
+let sum=(a,b)=>{
+    console.log(a,"/",b,"=",a/b);
+}
+sum(9,4)
+
+
+
+function item (a,b){
+    let rb = a+b
+    return rb
+}
+console.log(item(9,8));
+
+
+function item (a,b){
+    let rb =a-b
+    return rb
+}
+console.log(item(10,4));
+
+function bithi (a,b){
+    let bc=a*b
+    return bc
+}
+console.log(bithi(7,7));
+
+
+function lipi (a,b){
+    let cd = a/b
+    return cd
+}
+console.log(lipi(10,5));
